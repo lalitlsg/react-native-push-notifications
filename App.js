abe5668b6e97141e0496f9dc77f6a853dafae2d5
Lalit Garghate <lalit.garghate@gmail.com>
2021-04-24 18:58:14 +0530

    push notification, working with push noti server token

--- App.js
+++ App.js
@@ -1,18 +1,88 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
-import { StyleSheet, Text, View, Button } from "react-native";
+import React, { useEffect, useState } from "react";
+import { StyleSheet, View, Button } from "react-native";
 import * as Notifications from "expo-notifications";
+import * as Permissions from "expo-permissions";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => {
+    return {
+      shouldShowAlert: true,
+    };
+  },
+});
 
 export default function App() {
+  const [pushToken, setPushToken] = useState();
+
+  useEffect(() => {
+    Permissions.getAsync(Permissions.NOTIFICATIONS)
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          return Permissions.askAsync(Permissions.NOTIFICATIONS);
+        }
+        return statusObj;
+      })
+      .then((statusObj) => {
+        if (statusObj.status !== "granted") {
+          throw new Error("Permission Not Granted");
+        }
+      })
+      .then(() => {
+        return Notifications.getExpoPushTokenAsync();
+      })
+      .then((response) => {
+        const token = response.data;
+        setPushToken(token);
+      })
+      .catch(() => {
+        return null;
+      });
+  }, []);
+
+  useEffect(() => {
+    const foregroundSubscription = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        // console.log(notification);
+      }
+    );
+
+    const backgroundSubscription = Notifications.addNotificationResponseReceivedListener(
+      (response) => {
+        console.log(response);
+      }
+    );
+
+    return () => {
+      foregroundSubscription.remove();
+      backgroundSubscription.remove();
+    };
+  }, []);
+
   const notificationHandler = () => {
-    Notifications.scheduleNotificationAsync({
-      content: {
-        title: "Notification",
-        body: "This is notification",
+    // Notifications.scheduleNotificationAsync({
+    //   content: {
+    //     title: "Notification",
+    //     body: "This is notification",
+    //   },
+    //   trigger: {
+    //     seconds: 5,
+    //   },
+    // });
+
+    fetch("https://exp.host/--/api/v2/push/send", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Accept-Encoding": "gzip, deflate",
+        "Content-Type": "application/json",
       },
-      trigger: {
-        seconds: 5,
-      },
+      body: JSON.stringify({
+        to: pushToken,
+        data: { extraData: "SomeData" },
+        title: "Sent Via App",
+        body: "Testing via app",
+      }),
     });
   };
 
